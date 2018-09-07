9e338cf862a1e0bfcbdc2a6c6a5a8d1b73f7eb23
Aaron Goodman <aaronjgoodman@gmail.com>
2018-09-06 17:12:19 -0700

    more commits on R file

--- testing.R
+++ testing.R
@@ -14,9 +14,15 @@ sm <- stan_model("p1_m1_Gmix.stan")
 
 fit <- sampling(sm,data=lrgs.data,iter=4000,chains=2, control=list(max_treedepth=10))
 
+fit <- sampling(sm,data=lrgs.data,iter=4000,chains=2, control=list(max_treedepth=10,adapt_delta=0.9))
+
 pairs(fit,pars=c("alpha","beta","Sigma"))
 traceplot(fit,pars=c("alpha","beta","Sigma","lp__"),inc_warmup=TRUE)
-traceplot(fit,pars=c("pi","mu","Tau"),inc_warmup=TRUE)
-traceplot(fit,pars=c("mu0","mu"),inc_warmup=TRUE)
+traceplot(fit,pars=c("pi","mu","Tau"),inc_warmup=FALSE)
+traceplot(fit,pars=c("mu0","mu"),inc_warmup=FALSE)
 traceplot(fit,pars=c("mu0","Tau","U","W","lp__"))
        
+traceplot(fit,pars=c("alpha","beta","Sigma","mu_raw","mu0","log_Tau","log_U","log_W"))
+pairs(fit,pars=c("alpha","beta","Sigma","mu_raw","mu0","log_Tau","log_U","log_W"))
+
+pairs(fit,pars=c("alpha","beta","Sigma","mu_raw","mu0","log_Tau_raw","log_U_raw","log_W_raw",'xi[1]'))
